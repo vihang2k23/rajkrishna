e5c7ab12021d063636a351f02aa9249e307bbdb5
Vihang Patel <vihangp@zignuts.com>
2025-09-18 19:18:31 +0530

    change the slogan

--- app/app.vue
+++ app/app.vue
@@ -18,7 +18,7 @@ useHead({
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { name: 'description', content: 'Rajkrishna - Sustainable renewal solutions' }
+    { name: 'description', content: 'Rajkrishna - Engineering Energy with Precision' }
   ],
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/logo.png' }
